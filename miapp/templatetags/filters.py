b1d600b588bd80a1499c03af1790cb275f7f3b6a
Jaime Matheus <mjaimeg33@gmail.com>
2023-06-15 16:35:00 -0500

    Semana11

---
+++ miapp/templatetags/filters.py
@@ -0,0 +1,6 @@
+from django import template
+register = template.Library()
+@register.filter(name='saludo')
+def saludo(valor):
+    return f"<h1 style='background:green; color:white;'> Bienvenido, {valor} </h1>"
+ 
